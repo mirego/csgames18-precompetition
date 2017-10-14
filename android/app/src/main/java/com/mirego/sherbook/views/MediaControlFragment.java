1a9873d7e773602c20ddcd85646df8922fade24b
Unknown <jean.pascal.mcgee@gmail.com>
2017-10-14 13:57:38 -0400

    Binded UI elements of media_control
    Used butterknife to bind media_control Layout elements to the java file

--- android/app/src/main/java/com/mirego/sherbook/views/MediaControlFragment.java
+++ android/app/src/main/java/com/mirego/sherbook/views/MediaControlFragment.java
@@ -1,19 +1,41 @@
 package com.mirego.sherbook.views;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.mirego.sherbook.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MediaControlFragment extends Fragment {
     public MediaControlFragment() {
         // Required empty public constructor
     }
+
+    @BindView(R.id.media_control_seekbar)
+    SeekBar seekBar;
+
+    @BindView(R.id.media_control_play)
+    ImageButton PlayButton;
+
+    @BindView(R.id.media_control_name)
+    TextView Name;
+
+    @BindView(R.id.media_control_time)
+    TextView Time;
+
+    @BindView(R.id.media_control_image)
+    ImageView Image;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,5 +43,10 @@ public class MediaControlFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_media_control, container, false);
         ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @OnClick(R.id.media_control_play)
+    public void onPlayButtonPressed(View view) {
+        Snackbar.make(view, R.string.not_implemented_yet, Snackbar.LENGTH_SHORT).show();
     }
 }
