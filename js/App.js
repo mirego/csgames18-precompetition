9e954543405e2a76bb52767ec7cdfeaf0e28d00b
IMFUZZ <da.junior.du@gmail.com>
2017-10-14 14:42:34 -0400

    Swiper tested, added as app attribute

--- js/App.js
+++ js/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Swiper from 'react-native-swiper';
 
 import Header from './containers/Header';
@@ -11,14 +11,35 @@ import Contacts from './components/Contacts';
 import Settings from './components/Settings';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "selectedTabIndex": 0
+    };
+    this._swiper = null;
+  }
+
+  setSwiper(_swiper) {
+    this._swiper = _swiper;
+  }
+
+  handleSwiperIndexChange(index) {
+    // TODO: handle index change
+    console.log(this._swiper != null);
+  }
+
   render() {
     return (
       <View style={ styles.container }>
         <Header>
           <NavMenu />
         </Header>
+        <Button
+          onPress={() => { this._swiper.scrollBy(2) }}
+          title="Press Me"
+        />
         <Content>
-          <Swiper showsPagination={false} onIndexChange={(index) => { this.index = 0 } } >
+          <Swiper ref={(_swiper) => { this._swiper = _swiper; }} onIndexChanged={this.handleSwiperIndexChange} loop={true} showsPagination={false}>
             <Feed />
             <Messages />
             <Contacts />
